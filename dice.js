2222d39bab95c7f5b77f054879637c98109e0160
Otaroghene Odafejirorosua <favour.efado@gmail.com>
2026-05-05 21:24:33 +0100

    Add files via upload

---
+++ dice.js
@@ -0,0 +1,27 @@
+let btn1 = document.querySelector("button");
+let btn2 = document.querySelector(".btn2");
+let player1Img = document.querySelector(".l");
+let player2Img = document.querySelector(".r");
+let h2Element = document.querySelector("h2");
+
+btn1.addEventListener("click", function () {
+  let p1 = Math.floor(Math.random() * 6) + 1;
+  let p2 = Math.floor(Math.random() * 6) + 1;
+
+  player1Img.src = "./images/dice" + p1 + ".png";
+  player2Img.src = "./images/dice" + p2 + ".png";
+
+  if (p1 > p2) {
+    h2Element.textContent = "Player 1 Wins!";
+  } else if (p2 > p1) {
+    h2Element.textContent = "Player 2 Wins!";
+  } else {
+    h2Element.textContent = "Draw";
+  }
+});
+
+btn2.addEventListener("click", function () {
+  h2Element.textContent = "Roll dice!";
+  player1Img.src = "./images/dice1.png";
+  player2Img.src = "./images/dice1.png";
+});
